aebceecae0cabfc718ae8ab1c062ba9b53382e28
smita1017 <smita126@outlook.com>
2025-05-15 18:22:58 +0530

    new commit

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,     // Your Gmail address
-    pass: process.env.EMAIL_PASS      // Your App Password
+    user: process.env.EMAIL_USER,     //  Gmail address
+    pass: process.env.EMAIL_PASS      //  App Password
   }
 });
 
